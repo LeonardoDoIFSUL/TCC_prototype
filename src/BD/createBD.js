aeb7155f2bb7e8689d042824f823976afbf7e839
Leonardo Lucas <leonardolsm.oficial@gmail.com>
2023-04-16 11:13:23 -0300

    new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   src/BD/createBD.js 	new file:   src/BD/createTable.js 	new file:   src/BD/server.js 	new file:   src/views/home.ejs 	new file:   src/views/modelos/bodys.ejs 	new file:   src/views/modelos/footers.ejs 	new file:   src/views/modelos/headers.ejs

---
+++ src/BD/createBD.js
@@ -0,0 +1,16 @@
+const mySQL = require('mysql')
+const con = mySQL.createConnection({
+    host:'localhost', //TEM QUE MUDAR ISSO PELO ENDEREÇO E CRIAR O BANCO REAL NO PC
+    user: 'root', // SEMPRE LOGAR COMO ROOT
+    password:'1805', //IMPLEMTENTAR ESSA SENHA NO BANCO
+})
+
+con.connect(function (err){
+    if(err) throw err
+    let sql = "CREATE DATABASE TCC_prototype"
+    con.query(sql, function(err, result){
+        if(err) throw err
+        console.log("DataBase Criado com Sucesso")
+    })
+    con.end()
+})
